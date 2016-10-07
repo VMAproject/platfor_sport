2d28aca9e3280f3812105b740efe8c0750490272
bjj <artyrgetman@gmail.com>
2016-10-07 15:39:08 +0300

    change name of class

--- sport/src/main/java/com/sport/mvc/Controllers/smoll_fintess/A_PersonsController.java
+++ sport/src/main/java/com/sport/mvc/Controllers/smoll_fintess/A_PersonsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/registerPerson/")
-public class PersonsController {
+public class A_PersonsController {
 
 
     @RequestMapping("/general_registration_form")
