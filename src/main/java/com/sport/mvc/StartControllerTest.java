b598d9e7d765386b1031c446c4481a6142cd5487
bjj <artyrgetman@gmail.com>
2016-10-07 16:36:16 +0300

    refact repozotor

---
+++ src/main/java/com/sport/mvc/StartControllerTest.java
@@ -0,0 +1,16 @@
+package com.sport.mvc;
+
+import static org.junit.Assert.*;
+
+public class StartControllerTest {
+
+    @org.junit.Test
+    public void testShowIndexStartPage() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testShowChooseForm() throws Exception {
+
+    }
+}
